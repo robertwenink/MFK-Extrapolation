fixed faults in variance correlation weighing

proposed_method.py
@@ -76,7 +76,7 @@ def Kriging_unknown_z(x_b, X_unique, z_pred, Z_k):
         integration of standard normal gaussian function lamb"""
         c1 = z_pred - z1_b
         c2 = z1_b - z0_b
-        c3 = (s1_b + s0_b)/2 # NOTE should be s1_b - s0_b if they are assumed to move in the same direction
+        c3 = (s1_b + s0_b) # NOTE should be s1_b - s0_b if they are assumed to move in the same direction
         b1 = (c1 - lamb * s1_b) / (c2 + lamb * c3 + np.finfo(np.float32).eps)
 
         # scipy.stats.norm.pdf(lamb) == np.exp(-x**2 / 2) / np.sqrt(2*np.pi)
@@ -94,18 +94,17 @@ def Kriging_unknown_z(x_b, X_unique, z_pred, Z_k):
         return (b1 - Exp_b1) ** 2 * np.exp(-(lamb ** 2) / 2) / np.sqrt(2 * np.pi)
 
     y2 = var_b1(lambs)
-    Var_b1 = np.trapz(y2, lambs, axis=0)
+    Var_b1 = np.sqrt(np.trapz(y2, lambs, axis=0))
 
     # retrieve the (corrected) prediction + std
     # NOTE this does not retrieve z_pred at x_b if sampled at kriged locations.
     Z2_p = Exp_b1 * (Z1 - Z0) + Z1
 
-    # TODO taking S0 + S1 etc is not correct
     # TODO add extra term based on distance.
     # NOTE (Eb1+s_b1)*(E[Z1-Z0]+s_[Z1-Z0]), E[Z1-Z0] is just the result of the Kriging 
     # with s_[Z1-Z0] approximated as S1 + S0 for a pessimistic always oppositely moving case
-    Var_b2 = (S1 + S0)/2
-    S2_p = Exp_b1 * Var_b2 + (Z1 - Z0) * Var_b1 + Var_b2 * Var_b1 + S1
+    Var_b2 = (S1 + S0)
+    S2_p = abs(Exp_b1) * Var_b2 + abs(Z1 - Z0) * Var_b1 + Var_b2 * Var_b1 + S1
 
     # get index in X_unique of x_b
     ind = X_unique == x_b
@@ -163,7 +162,8 @@ def weighted_prediction(X, X_unique, Z, Z_k):
     #    This decreases distance based influence if sigma > 0.
     #    NOTE TODO this is not (yet) a fully math-informed decision in terms of efffectiveness.
 
-    c = c / np.exp(-D_mse)
+    mult = np.exp(-D_mse)
+    c = c * mult
 
     " Scale to sum to 1; correct for sampled locations "
     # Contributions coefficients should sum up to 1.

proposed_method_main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     " inits and settings"
     X, Z, Z_k, costs, Z_pred = [], [], [], [], []
     n_samples_l0 = 10
-    max_cost = 200
+    max_cost = 300
     l = 0
     max_level = l + 2
     runEGO = True
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     # draw result
     X_plot = np.arange(0, 1 + np.finfo(np.float32).eps, 1 / 200)
     ax = draw_current_levels(X, Z, Z_k, X0, X_plot, solver)
-
+    
     print("Initial cost: {}".format(np.sum(costs)))
     while np.sum(costs) < max_cost and l <= max_level:
         "sample first point at new level"
@@ -66,6 +66,7 @@ if __name__ == "__main__":
         # sample the best location, increase costs
         z_pred, cost = mf_forrester2008(x_b, l, solver)
         costs.append(cost)
+        print("Current cost: {}".format(np.sum(costs)))
 
         # add initial sample on the level to known list of samples
         X.append([x_b])
@@ -80,7 +81,6 @@ if __name__ == "__main__":
         Z_k_new = Kriging(X_unique, Z_new_p, hps=Z_k[-1].hps)
 
         " output "
-        print("Current cost: {}".format(np.sum(costs)))
         ax = draw_current_levels(X, Z, [*Z_k, Z_k_new], X_unique, X_plot, solver, ax)
 
         " sample from the predicted distribution in EGO fashion"
@@ -108,6 +108,7 @@ if __name__ == "__main__":
             X[-1].append(x_new)
             Z[-1].append(z_new)
             costs.append(cost)
+            print("Current cost: {}".format(np.sum(costs)))
 
             # recalculate X_unique
             X_unique = return_unique(X)
@@ -150,9 +151,8 @@ if __name__ == "__main__":
             
             # " output "
             ax = draw_current_levels(
-                X, Z, [*Z_k, Z_k_new, dir], X_unique, X_plot, solver, ax, Z_k_new_noise
+                X, Z, [*Z_k, Z_k_new], X_unique, X_plot, solver, ax, Z_k_new_noise
             )
-            print("Current cost: {}".format(np.sum(costs)))
 
 
         # update kriging models of levels and discrepancies before continueing to next level