corrected variance weighing, changing saving of X to accomodate MF.

models/kriging/method/OK.py
@@ -63,9 +63,12 @@ def _predictor(R_in, r, y, mu_hat):
 
 @njit(cache=True)
 def _sigma_mu_hat(R_in, y, n):
-    # NOTE +np.finfo(np.float32).eps makes the function more stable, and wont change the max hps
-    t = y - np.sum(R_in * y) / (np.sum(R_in)+np.finfo(np.float32).eps)
-    return np.dot(t.T, np.dot(R_in, t)) / n
+    if np.linalg.cond(R_in) < 1e8: # NOTE quite large
+        # NOTE +np.finfo(np.float32).eps makes the function more stable, and wont change the max hps
+        t = y - np.sum(R_in * y) / (np.sum(R_in)+np.finfo(np.float32).eps)
+        return -n * np.log(np.dot(t.T, np.dot(R_in, t)) / n)
+    else:
+        return -1e8
 
 
 @njit(cache=True)
@@ -76,7 +79,7 @@ def _fitness_func_loop(R_in_list, y, R):
     n = y.shape[0]
     for i in range(n_pop):
         # MLE, omitting factor 1/2
-        fit[i] = -n * np.log(_sigma_mu_hat(R_in_list[i], y, n)) - np.linalg.slogdet(R[i])[1]
+        fit[i] = _sigma_mu_hat(R_in_list[i], y, n) - np.linalg.slogdet(R[i])[1]
     return fit
 
 
@@ -162,7 +165,7 @@ class OrdinaryKriging:
         
         t = time.time() - start
 
-        # assign results to Kriging object
+        # assign results to Kriging object, this sets hps_init for the tuning as well.
         self.hps = self.model.output_dict["variable"].reshape(self.hps.shape)
 
         # print results of tuning

postprocessing/plotting.py
@@ -152,7 +152,7 @@ class Plotting:
 
         " plot prediction surface "
         ax, ind = self.axes[ind], ind + 1
-        ax.plot_surface(*self.X_plot, y_hat, alpha=0.9, color=self.color, label=label)
+        self.fix_colors(ax.plot_surface(*self.X_plot, y_hat, alpha=0.9, color=self.color, label=label))
         ax.plot_surface(*self.X_plot, y_hat - 2 * std, alpha=0.4, color=self.color)
         ax.plot_surface(*self.X_plot, y_hat + 2 * std, alpha=0.4, color=self.color)
 
@@ -170,7 +170,7 @@ class Plotting:
 
             # plot
             ax, ind = self.axes[ind], ind + 1
-            ax.plot_surface(*self.X_plot, y_exact, alpha=0.9)
+            self.fix_colors(ax.plot_surface(*self.X_plot, y_exact, alpha=0.9))
 
             # add samplepoints
             ax.scatter(*X[:, self.d_plot].T, y, c=self.color, marker=self.marker)
@@ -212,6 +212,10 @@ class Plotting:
         ax.plot(*self.X_plot, y_hat + 2 * std, color=self.color, alpha=0.4)
         ax.plot(*self.X_plot, y_hat - 2 * std, color=self.color, alpha=0.4)
 
+    def fix_colors(self,surf):
+        surf._facecolors2d = surf._facecolor3d
+        surf._edgecolors2d = surf._edgecolor3d
+
     def draw_current_levels(self, X, Z_k, X_unique):
         """
         @param X: !! 3D array, different in format, it is now an array of multiple X in the standard format
@@ -234,8 +238,12 @@ class Plotting:
         " plot prediction kriging "
         # first two levels always known
         if len(X) > 2:
-            # elevate level and plot our prediction, estimated points in black
             i += 1
+
+            # prediction line, this is re-interpolated if we used noise and might not cross the sample points exactly
+            self.plot(X[i], Z_k[i], label="Kriging level {}".format(i))
+            
+            # plot our prediction, estimated points in black
             self.axes[0].scatter(
                 *X_unique[:, self.d_plot].T,
                 Z_k[i].predict(self.transform_X(X_unique))[0],
@@ -244,50 +252,52 @@ class Plotting:
                 label = "Extrapolated points"
             )
 
-            # prediction line, this is re-interpolated if we used noise and might not cross the sample points exactly
-            self.plot(X[i], Z_k[i], label="Kriging level {}".format(i))
-
             # best out of all the *sampled* locations
             Z = Z_k[-1].predict(X[-1])[0]
             best = np.argmin(Z)
             t =  X[i][best, self.d_plot].T
             self.axes[0].scatter(*X[i][best, self.d_plot].T, Z[best], s = 60, marker = "*", color = 'red', zorder = 10, label="Current best")
 
+            if self.plot_exact:
+                # exact result of the level we try to predict
+                y_pred_truth = self.solver.solve(self.X_pred, l=i+2)[0].reshape(
+                    self.X_plot[0].shape
+                )
+                kwargs = {
+                    "label":"true level {}".format(i),
+                    "color":self.color,
+                    "alpha":0.5
+                }
+                if self.d == 1:
+                    ax=self.axes[0]
+                    ax.plot(*self.X_plot, y_pred_truth, '--', **kwargs ) 
+                else:
+                    ax=self.axes[1]
+                    self.fix_colors(ax.plot_surface(*self.X_plot, y_pred_truth, **kwargs))
+
+            
         " plot truth "
         if self.plot_exact:
-            # exact result of the level we try to predict
-            y_pred_truth = self.solver.solve(self.X_pred, l=i)[0].reshape(
-                self.X_plot[0].shape
-            )
-
             # exact hifi truth
             y_exact = self.solver.solve(self.X_pred)[0].reshape(self.X_plot[0].shape)
 
-            # use correct plotting function
-            kwargs = {
-                "label":"true level {}".format(i),
-                "color":self.color,
-                "alpha":0.5
-            }
             kwargs2 = {
                 "label":"truth", 
                 "color":"black", 
                 "alpha": 0.3
             }
+            
+            # use correct plotting function
             if self.d == 1:
                 ax=self.axes[0]
-                ax.plot(*self.X_plot, y_pred_truth, '--', **kwargs ) 
                 ax.plot(*self.X_plot, y_exact, '--', **kwargs2)
             else:
                 ax=self.axes[1]
-                ax.plot_surface(*self.X_plot, y_pred_truth, **kwargs) 
-                ax.plot_surface(*self.X_plot, y_exact, **kwargs2)
+                self.fix_colors(ax.plot_surface(*self.X_plot, y_exact, **kwargs2))
+                
         
-        # for ax in self.axes:
-        #     for line in ax._get_lines:
-        #         line._facecolors2d=surf._facecolors3d
-        #         line._edgecolors2d=surf._edgecolors3d
-        #     ax.legend()
+        for ax in self.axes:
+            ax.legend()
 
         plt.draw()
         plt.pause(1)

preprocessing/input.py
@@ -72,6 +72,14 @@ class Input:
         f.write(self.filename)
         f.close()
 
+    def read_X(self):
+        if hasattr(self,'X'):
+            for i in range(len(self.X)):
+                # this implies we always should define our X here as 3d list/array
+                self.X[i] = np.array(self.X[i],dtype=np.float64)
+                assert(self.X[i].ndim == 2), "not retrieving a 2 dimensional X!"
+
+
     def read_input(self):
         """Read the json file inputs and converting it to class attributes"""
         file_path = os.path.join(INPUTS_DIR, self.filename)
@@ -85,8 +93,7 @@ class Input:
             setattr(self, key, data_dict[key])
         
         # NOTE this part is a bit of hardcoding, in order to convert some lists back to np.array    
-        if hasattr(self,'X'):
-            self.X = np.array(self.X,dtype=np.float64)
+        self.read_X()
         self.search_space[1] = np.array(self.search_space[1])
         self.search_space[2] = np.array(self.search_space[2])
         

proposed_method.py
@@ -43,40 +43,41 @@ def Kriging_unknown_z(x_b, X_unique, z_pred, Z_k):
     Z1, S1 = Z1_k.predict(X_unique)
     S0, S1 = np.sqrt(S0), np.sqrt(S1)
 
-    def exp_b1(lamb):
+    def exp_f(lamb):
         """get expectation of the function according to
         integration of standard normal gaussian function lamb"""
         c1 = z_pred - z1_b
         c2 = z1_b - z0_b
-        c3 = (s1_b + s0_b) # NOTE should be s1_b - s0_b if they are assumed to move in the same direction
-        b1 = (c1 - lamb * s1_b) / (c2 + lamb * c3 + np.finfo(np.float32).eps)
+        c3 = abs(s1_b - s0_b) # NOTE should be s1_b - s0_b if they are assumed to move in the same direction
+        f = (c1 - lamb * s1_b) / (c2 + lamb * c3 + np.finfo(np.float64).eps)
 
         # scipy.stats.norm.pdf(lamb) == np.exp(-x**2 / 2) / np.sqrt(2*np.pi)
-        return b1 * np.exp(-(lamb ** 2) / 2) / np.sqrt(2 * np.pi), b1
+        return f * np.exp(-(lamb ** 2) / 2) / np.sqrt(2 * np.pi), f
 
     # 1000 steps; evaluates norm to 0.9999994113250346
     lambs = np.arange(-5, 5, 0.01)
 
     # evaluate expectation
-    y, b1 = exp_b1(lambs)
-    Exp_b1 = np.trapz(y, lambs, axis=0)
+    Y, f = exp_f(lambs)
+    Ef = np.trapz(Y, lambs, axis=0)
 
-    def var_b1(lamb):
-        # E[(X-E[X])]
-        return (b1 - Exp_b1) ** 2 * np.exp(-(lamb ** 2) / 2) / np.sqrt(2 * np.pi)
+    def var_f(lamb):
+        # Var X = E[(X-E[X])**2]
+        # down below: random variable - exp random variable, squared, times the pdf of rho (N(0,1)); 
+        # we integrate this function and retrieve the expectation of this expression, i.e. the variance.
+        return (f - Ef) ** 2 * np.exp(-(lamb ** 2) / 2) / np.sqrt(2 * np.pi)
 
-    y2 = var_b1(lambs)
-    Var_b1 = np.sqrt(np.trapz(y2, lambs, axis=0)) # NOTE not a normal gaussian variance, but we use it as such
+    Y = var_f(lambs)
+    Sf = np.sqrt(np.trapz(Y, lambs, axis=0)) # NOTE not a normal gaussian variance, but we use it as such
 
     # retrieve the (corrected) prediction + std
     # NOTE this does not retrieve z_pred at x_b if sampled at kriged locations.
-    Z2_p = Exp_b1 * (Z1 - Z0) + Z1
+    Z2_p = Ef * (Z1 - Z0) + Z1
 
     # TODO add extra term based on distance.
     # NOTE (Eb1+s_b1)*(E[Z1-Z0]+s_[Z1-Z0]), E[Z1-Z0] is just the result of the Kriging 
     # with s_[Z1-Z0] approximated as S1 + S0 for a pessimistic always oppositely moving case
-    Var_b2 = abs(S1 - S0)
-    S2_p = abs(Exp_b1) * Var_b2 + abs(Z1 - Z0) * Var_b1 + Var_b2 * Var_b1 + S1
+    S2_p = S1 + abs((S1 - S0) * Ef) + abs(Z1 - Z0) * Sf 
 
     # get index in X_unique of x_b
     ind = np.all(X_unique == x_b, axis=1)
@@ -87,7 +88,7 @@ def Kriging_unknown_z(x_b, X_unique, z_pred, Z_k):
     # set S2_p to 0 at that index
     S2_p[ind] = 0
 
-    return Z2_p, S2_p ** 2
+    return Z2_p, S2_p ** 2, Sf ** 2
 
 
 def weighted_prediction(setup, X, X_unique, Z, Z_k):
@@ -113,11 +114,11 @@ def weighted_prediction(setup, X, X_unique, Z, Z_k):
     " Collect new results "
     # NOTE if not in X_unique, we could just add a 0 to all previous,
     # might be faster but more edge-cases
-    D, D_mse = [], []
+    D, D_mse, D_Sf = [], [], []
     for i in range(X_s.shape[0]):
-        Z_p, mse_p = Kriging_unknown_z(X_s[i], X_unique, Z_s[i], Z_k)
-        D.append(Z_p), D_mse.append(mse_p)
-    D, D_mse = np.array(D), np.array(D_mse)
+        Z_p, mse_p, Sf = Kriging_unknown_z(X_s[i], X_unique, Z_s[i], Z_k)
+        D.append(Z_p), D_mse.append(mse_p), D_Sf.append(Sf)
+    D, D_mse, D_Sf = np.array(D), np.array(D_mse), np.array(D_Sf)
 
     " Weighing "
     # 1) distance based: take the (tuned) Kriging correlation function
@@ -126,25 +127,26 @@ def weighted_prediction(setup, X, X_unique, Z, Z_k):
     km = Kriging(setup,X_s, None, hps_init=hps, train=False)
     c = km.corr(X_s, correct_formatX(X_unique,setup.d), hps)
 
-    # 2) variance based: predictions without variances involved are presumably more reliable
+    mask = c == 1.0  # then sampled point
+    idx = mask.any(axis=0)  # get columns with sampled points
+
+    # 2) variance based: predictions based on fractions without large variances Sf involved are more reliable
     #    we want to keep the correlation/weighing the same if there is no variance,
     #    and otherwise reduce it.
     #    We could simply do: sigma += 1 and divide.
     #    However, sigma is dependend on scale of Z, so we should better use e^-sigma.
     #    This decreases distance based influence if sigma > 0.
+    #    We take the variance of 
     #    NOTE TODO this is not (yet) a fully math-informed decision in terms of efffectiveness.
 
-    mult = np.exp(-D_mse)
-    c = c * mult
+    mult = np.exp(-D_Sf)
+    c = (c.T * mult).T
 
     " Scale to sum to 1; correct for sampled locations "
     # Contributions coefficients should sum up to 1.
     # Furthermore, sampled locations should be the only contribution for themselves.
     #  if we dont do this, EI does not work!
     #  however, we might get very spurious results -> we require regression!
-
-    mask = c == 1.0  # then sampled point
-    idx = mask.any(axis=0)  # get columns with sampled points
     c[:, idx] = 0  # set to zero
     c = c + mask  # retrieve samples exactly
 

proposed_method_main.py
@@ -13,12 +13,16 @@ from utils.data_utils import return_unique
 setup = Input(0)
 solver = get_solver(setup)
 doe = get_doe(setup)
+if hasattr(setup,'X'):
+    X = setup.X # this does not work for MF yet!
+else:
+    X = doe(setup, 10*setup.d)
 
 pp = Plotting(setup)
 
 " inits and settings"
 X, Z, Z_k, costs, Z_pred = [], [], [], [], []
-n_samples_l0 = 20
+n_samples_l0 = 10
 max_cost = 1000
 l = 0
 max_nr_levels = 3
@@ -46,7 +50,7 @@ X_unique, _ = return_unique(X)
 
 # create Krigings of levels, same hps
 Z_k.append(Kriging(setup, X[0], Z[0], tuning=True))
-Z_k.append(Kriging(setup, X[1], Z[1], hps_init=Z_k[0].hps))
+Z_k.append(Kriging(setup, X[1], Z[1], hps_init=Z_k[0].hps, tuning = True))
 
 # draw result
 pp.draw_current_levels(X, Z_k, X_unique)
@@ -72,7 +76,7 @@ while np.sum(costs) < max_cost and len(X) < max_nr_levels:
     X_unique, X_unique_exc = return_unique(X,X[-1])
 
     " predict based on single point"
-    Z_new_p, mse_new_p = Kriging_unknown_z(x_b, X_unique, z_pred, Z_k)
+    Z_new_p, mse_new_p, _ = Kriging_unknown_z(x_b, X_unique, z_pred, Z_k)
     Z_k_new = Kriging(setup, X_unique, Z_new_p, hps_init=Z_k[-1].hps)
 
     " output "
@@ -82,7 +86,7 @@ while np.sum(costs) < max_cost and len(X) < max_nr_levels:
     ei = 1
     ei_criterion = 2 * np.finfo(np.float32).eps
     while np.sum(costs) < max_cost:
-        # select points to asses expected
+        # select points to asses expected improvement
         X_infill = pp.X_pred # TODO does not work for d>2
 
         # predict and calculate Expected Improvement
@@ -92,12 +96,12 @@ while np.sum(costs) < max_cost and len(X) < max_nr_levels:
         for i in range(len(y_pred)):
             ei[i] = EI(y_min, y_pred[i], sigma_pred[i])
 
-        # terminate if criterion met
-        if np.all(ei < ei_criterion):
-            break
-
         # select best point to sample
         x_new = correct_formatX(X_infill[np.argmax(ei)],setup.d)
+
+        # terminate if criterion met, or for some buggy reason we revisit a point
+        if np.all(ei < ei_criterion) or x_new in X[-1]:
+            break
 
         # sample, append lists
         z_new, cost = solver.solve(x_new, l)
@@ -156,6 +160,10 @@ while np.sum(costs) < max_cost and len(X) < max_nr_levels:
 
     # moving to the next level we will be sampling at
     l += 1
+
+if setup.SAVE_DATA:
+    setup.X = X
+    setup.create_input_file()
 
 print("Simulation finished")
 show = True

sampling/solvers/internal.py
@@ -142,17 +142,17 @@ class TestFunction(Solver):
 
                 if self.solver_noise:
                     # add multiplicative noise that is converging together with the convergence
-                    conv *= 1 + (1 - conv) * 0.01 * (
+                    conv *= 1 + (1 - conv) * 0.02 * (
                         np.random.standard_normal(size=conv.shape) - 0.5
                     )
 
                 " transformation "
                 # TODO scale contributions according to size of domain
-                # using abs(conv - 1) is a non-linear effect
+                # using abs(conv - 1) is a non-linear effect, but is required for mimicking Forrester/Meliani (in which case it is not non-linear??)
                 linear_gain_mod = 2
                 A = conv
-                B = 10 * (conv - 1) * linear_gain_mod
-                C = -5 * (conv - 1) * linear_gain_mod
+                B = 10 * abs(conv - 1) * linear_gain_mod
+                C = -5 * abs(conv - 1) * linear_gain_mod
 
                 val = A * val + B * (np.sum(X, axis=1) - 0.5) + C
 