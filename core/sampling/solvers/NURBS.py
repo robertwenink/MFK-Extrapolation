0470db775ebfb48808df5e363e61d98e1debbdc1
Robert Wenink <robertwenink@gmail.com>
2023-02-20 15:07:30 +0100

    add typeignore

--- core/sampling/solvers/NURBS.py
+++ core/sampling/solvers/NURBS.py
@@ -35,8 +35,8 @@ def create_curve_figure(crv, x=None, path=None):
     plt.gca().set_axis_off()
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
-    plt.gca().xaxis.set_major_locator(plt.NullLocator())
-    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.gca().xaxis.set_major_locator(plt.NullLocator()) # type: ignore    
+    plt.gca().yaxis.set_major_locator(plt.NullLocator()) # type: ignore    
     
     def get_concat_h(im1, im2):
         dst = Image.new('RGBA', (im1.width + im2.width, im1.height))
