33336ee4f8f33cf7cf11ace159eadfb1aeca3d6c
Robert Wenink <robertwenink@gmail.com>
2023-03-17 13:08:24 +0100

    Swapped name of stable up and down

--- core/sampling/solvers/convergence_base_models.py
+++ core/sampling/solvers/convergence_base_models.py
@@ -7,7 +7,7 @@ import numpy as np
 # Power describing the speed of convergence. Higher is faster
 CONVERGENCE_SPEED = 2
 
-def conv_stable_up(l):
+def conv_stable_down(l):
     """return sum of series reciprocals up to number l.
     p=2; Basel problem converges to pi**2 / 6"""
     p = 2
@@ -18,8 +18,8 @@ def conv_stable_up(l):
     return (np.pi ** 2 / 6 + mod) / (np.sum(np.divide(1, n ** 2)) + mod)
 
 
-def conv_stable_down(l):
-    return 1 / conv_stable_up(l)
+def conv_stable_up(l):
+    return 1 / conv_stable_down(l)
 
 
 def conv_alternating(l):
