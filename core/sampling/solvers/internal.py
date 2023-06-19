RMSE fix from normalise with average to range

core/sampling/solvers/internal.py
@@ -93,7 +93,7 @@ class TestFunction(Solver):
             self.value_metrics = [np.min(Z), np.max(Z), np.mean(Z)]
 
             self.solver_noise = setup.solver_noise
-            if self.solver_noise == True: # bcs the one from setup is actually a boolean
+            if self.solver_noise == True: # bcs the one from setup is usually a boolean
                 self.solver_noise = 0.02
 
             if self.mf:
@@ -131,24 +131,24 @@ class TestFunction(Solver):
                 3 (more non-linear variant)
         @returns self.conv_mod_func(X,l)
         """
-
+        nr_of_periods = 2
         if conv_mod == 1:
             return lambda X, l: conv_base_func(l) + 2 * (1 - conv_base_func(l)) * (
-                np.sin(X_acc_func(X) * 2 * np.pi)
+                np.sin(X_acc_func(X) / (np.sum(self.bound_range)) * nr_of_periods * 2 * np.pi)
             )
         elif conv_mod == 2:
             return lambda X, l: conv_base_func(l) + (1 - conv_base_func(l)) * (
                 np.sin(
                     # bij problemen ooit met negatieve power: np.sign(conv_base_func(l))*np.abs((conv_base_func(l)+0j)**(1/2)))
                     (conv_base_func(l))**(1/8)
-                    * X_acc_func(X)
+                    * X_acc_func(X) / (np.sum(self.bound_range)) * nr_of_periods
                     * 2
                     * np.pi
                 )
             )
         elif conv_mod == 3:
             return lambda X, l: conv_base_func(l) + (1 - conv_base_func(l)) * (
-                np.sin((conv_base_func(l))**(1/2) * X_acc_func(X) * 2 * np.pi)
+                np.sin((conv_base_func(l))**(1/4) * X_acc_func(X) / (np.sum(self.bound_range)) * nr_of_periods * 2 * np.pi)
             )
         else:
             return lambda X, l: conv_base_func(l) * np.ones(X.shape[0])
@@ -280,6 +280,39 @@ class Forrester2008(TestFunction):
     def get_optima(self):
         return correct_formatX([[0.757249]],self.d), [-6.02074]
 
+class Branin_Perdikaris(TestFunction):
+    max_d = 2
+    min_d = 2
+    name = "Branin_Perdikaris"
+
+    def hifi(self, x1, x2):
+        return ((-1.275 * x1**2)/np.pi**2 + 5*x1/np.pi + x2 -6)**2 + (10 - 5/(4*np.pi))*np.cos(x1) + 10
+    
+    def mefi(self, x1, x2):
+        return 10*np.sqrt(self.hifi(x1-2,x2-2)) + 2*(x1 - 0.5) - 3*(3*x2 -1) -1
+
+    def lofi(self, x1, x2):
+        return self.mefi(1.2*(x1+2), 1.2*(x2+2)) - 3 * x2 + 1
+
+    def solve(self, X, l = None):
+        X = self.check_format_X(X)
+        x1 = X[:, 0]
+        x2 = X[:, 1]
+
+        if l == 1:
+            return self.lofi(x1,x2), 1
+        if l == 2:
+            return self.mefi(x1,x2), 1
+        if l == 3 or l == None:
+            return self.hifi(x1,x2), 1
+
+    @staticmethod
+    def get_preferred_search_space(d):
+        return [["x0", "x1"], [-5, 0], [10, 15]]
+    
+    def get_optima(self):
+        # return correct_formatX([[-np.pi,12.275],[np.pi,2.275],[9.42478,2.475]],self.d), 0.397887
+        return correct_formatX([[-np.pi,12.275]],self.d), [0.397887]
 
 class Branin(TestFunction):
     max_d = 2

main_mf.py
@@ -66,10 +66,10 @@ setup = Input(0)
 
 conv_mods = [0, 1, 2, 3]
 conv_types = ["Stable up", "Stable down", "Alternating"]
-solver_noises = [0.0, 0.02, 0.05, 0.1]
+solver_noises = [0.0, 0.02, 0.05]
 
 setup.conv_mod = conv_mods[1]
-setup.conv_type = conv_types[0]
+setup.conv_type = conv_types[1]
 setup.solver_noise = solver_noises[0]
 
 reuse_values = False
@@ -89,14 +89,14 @@ MFK_kwargs = {'print_global' : False,
   
 # NOTE zonder noise werkt zonder optim var beter voor reference
 # mf_model = MFK_smt(setup, max_cost = 150000, initial_nr_samples = 1, **MFK_kwargs)# NOTE cant use one (1) because of GLS in smt!
-mf_model = MultiFidelityEGO(setup, proposed = True, optim_var = True, initial_nr_samples = 3, max_cost = np.inf, MFK_kwargs = MFK_kwargs)
+mf_model = MultiFidelityEGO(setup, proposed = True, optim_var = True, initial_nr_samples = 2, max_cost = np.inf, MFK_kwargs = MFK_kwargs)
 # mf_model = ProposedMultiFidelityKriging(setup, max_cost = 150000, initial_nr_samples = 1, MFK_kwargs = MFK_kwargs)
 
 mf_model.distance_weighing = True
 mf_model.variance_weighing = True
 
 mf_model.method_weighing = False
-mf_model.try_use_MFK = True
+mf_model.try_use_MFK = False
 
 mf_model.use_uncorrected_Ef = False
 mf_model.use_het_noise = False
@@ -105,20 +105,23 @@ mf_model.use_het_noise = False
 mf_model.set_L([0.5, 1, 2])
 
 if isinstance(get_solver(setup),TestFunction):
-    mf_model.set_L([1, 2, None])
+    mf_model.set_L([2, 3, None])
+    # mf_model.set_L([1, 2, None])
     # mf_model.set_L([0, 2, None])
     mf_model.set_L_costs([1,10,1000])   
 
 
 # init plotting etc
 pp = Plotting(setup, plotting_pause = 0.001, plot_once_every=1, fast_plot=False, make_video=True)
+pp.plot_exact_new_figure()
+# plt.show()
 # pp.set_zoom_inset([0,3], x_rel_range = [0.05,0.2])
 pp.plot_NRMSE_text = True
 cp = ConvergencePlotting(setup)
 
 
 
-" level 0 and 1 : setting 'DoE' and 'solve' "
+" level 0 and 1 : setting 'DoE' and 'solve' " 
 used_endstate = hasattr(setup,'model_end') and hasattr(setup,'prepare_succes') and reload_endstate
 if used_endstate:
     mf_model.set_state(deepcopy(setup.model_end))
@@ -140,6 +143,9 @@ X_RMSE = LHS(setup, n_per_d=80)
 RMSE_focus = RMSE_focussed(mf_model, X_RMSE, 10)
 mf_model.print_stats(RMSE, RMSE_focus)
 print_pearson_correlations(mf_model)
+
+# plt.show()
+# sys.exit()
 
 " sample from the predicted distribution in EGO fashion"
 if isinstance(mf_model, MultiFidelityEGO):

utils/error_utils.py
@@ -9,8 +9,8 @@ def RMSE(Z, Z_predict):
     return RMSE
 
 
-def RMSE_norm(Z, Z_predict):
-    return RMSE(Z, Z_predict) / np.mean(np.abs(Z))
+def RMSE_norm(Z, Z_predict, Z_full):
+    return RMSE(Z, Z_predict) / (np.max(Z_full) - np.min(Z_full))
 
 
 def MAE_norm(Z, Z_predict):
@@ -32,9 +32,9 @@ def RMSE_norm_MF(mf_model, no_samples = False):
     for i, K_l in enumerate(mf_model.K_mf):
         if no_samples:
             inds = isin_indices(mf_model.X_truth,mf_model.X_mf[-1],inversed=True)
-            RMSE.append(RMSE_norm(mf_model.Z_truth[inds], K_l.predict(mf_model.X_truth[inds])[0]))
+            RMSE.append(RMSE_norm(mf_model.Z_truth[inds], K_l.predict(mf_model.X_truth[inds])[0], mf_model.Z_truth))
         else:
-            RMSE.append(RMSE_norm(mf_model.Z_truth, K_l.predict(mf_model.X_truth)[0]))
+            RMSE.append(RMSE_norm(mf_model.Z_truth, K_l.predict(mf_model.X_truth)[0], mf_model.Z_truth))
     
     return RMSE
 
@@ -63,8 +63,7 @@ def RMSE_focussed(mf_model, X_search, focus_perc):
     RMSE = []
     for K_l in mf_model.K_mf:
         # normalize with the full range, not the limited one!
-        RMSE_n = RMSE_norm(z_search_truth[mask], K_l.predict(X_search[mask])[0]) \
-            * np.mean(np.abs(z_search_truth[mask])) / np.mean(np.abs(z_search_truth))
+        RMSE_n = RMSE_norm(z_search_truth[mask], K_l.predict(X_search[mask])[0], mf_model.Z_truth) 
         RMSE.append(RMSE_n)
 
     return RMSE